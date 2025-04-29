521063e922fcdf24cd902965f13b7d540ed349ef
JordiGis <jorgisfer@alu.edu.gva.es>
2025-04-29 22:21:20 +0200

    ADD validacion de cargar preguntas

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,8 +1,14 @@
 <!-- src/views/GameView.vue -->
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
+    <div v-if="store.preguntas.length !== 0"
+        class="min-h-screen d-flex align-items-center justify-content-center bg-gray-100 dark:bg-gray-900 p-4">
         <SelectPlayers v-if="!store.numJugadores" />
         <Game v-else />
+    </div>
+    <div v-else
+        class="min-h-screen d-flex flex-column align-items-center justify-content-center bg-gray-100 dark:bg-gray-900 p-4">
+        <h1 class="m-3 mb-5">Primero se tienen que cargar las preguntas:</h1>
+        <button @click="goToLoadQuestions" class="btn btn-primary">Ir a Cargar Preguntas</button>
     </div>
 </template>
 
@@ -12,4 +18,11 @@ import Game from '../components/Game.vue'
 import { useGameStore } from '../stores/useGameStore'
 
 const store = useGameStore()
+
+import { useRouter } from 'vue-router'
+const $router = useRouter()
+
+function goToLoadQuestions() {
+    $router.push({ name: 'loadDilems' })
+}
 </script>
