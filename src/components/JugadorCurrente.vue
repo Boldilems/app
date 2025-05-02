ba06c7b8b41a3fcf350f6e40d53db26888c42920
JordiGis <jorgisfer@alu.edu.gva.es>
2025-05-02 02:44:50 +0200

    FIX estilo movil

--- src/components/JugadorCurrente.vue
+++ src/components/JugadorCurrente.vue
@@ -1,20 +1,26 @@
 <template>
     <!-- <div v-if="store.jugadores.length > 1" class="d-flex justify-content-center my-3"> -->
-    <div class="d-flex justify-content-center my-3">
-        <button id="changeJugador" title="Cambiar jugador" @click="store.lastJugador">
-            <i class="bi bi-arrow-left-right"></i>
-        </button>
-    </div>
-    <div class="w-100 text-white rounded shadow text-center current-player">
-        <h2 class="fw-bold p-2 titulo">Turno de:</h2>
-        <h1 class="display-5 p-3 fw-bold" :style="{ backgroundColor: store.jugadorCurrent.color || '#6c757d' }">
-            {{ store.jugadorCurrent.name }}
-        </h1>
+    <div id="jugador-currente">
+        <div id="changeJugador" class="d-flex justify-content-center my-3">
+            <button title="Cambiar jugador" @click="store.lastJugador">
+                <i class="bi bi-arrow-left-right"></i>
+            </button>
+        </div>
+        <div class="w-100 text-white rounded shadow text-center current-player">
+            <h2 class="fw-bold p-2 titulo">Turno de:</h2>
+            <h1 class="display-5 p-3 fw-bold" :style="{ backgroundColor: store.jugadorCurrent.color || '#6c757d' }">
+                {{ store.jugadorCurrent.name }}
+            </h1>
+        </div>
     </div>
 </template>
 
 <style scoped>
 #changeJugador {
+    flex-direction: column;
+}
+
+#changeJugador button {
     background-color: #0d6efd;
     /* Azul Bootstrap */
     border: none;
@@ -43,6 +49,14 @@
     border-bottom: 3px solid var(--darkreader-background-0d6efd, #134ea5) !important;
     background-color: var(--darkreader-background-dc3545, #8a242e) !important;
     margin-bottom: 0;
+}
+
+@media (max-width: 768px) {
+    #jugador-currente {
+        transform: scale(0.7);
+        width: 100%;
+    }
+
 }
 </style>
 
